c9cdf0007d7a8a1915eae40a89f0af695b2eae65
YINYL <yinyiling_ray@163.com>
2023-03-11 20:45:20 +0800

    feat: init project - first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+
+const Hero = () => {
+  return (
+    <section className=" relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] mx-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
+          <div className="w-1 sm:h-80 h-40 violet-gradient"></div>
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eff]"> YIN YILING</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I am a web developer.
+            <br className="sm:block hidden" />I can privide customized web
+            application for your own specific needs.
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
+  );
+};
+
+export default Hero;
